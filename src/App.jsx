148199c5f60f5f67f883f53e4cf99d2bd111290f
dresan22 <andresat48@hotmail.com>
2022-08-07 22:46:38 -0400

    remove slate bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import DarkMode from './components/darkmode/darkmode';
 function App() {
   return (
     <DarkMode>
-      <div className={`grid place-items-center h-screen  sm:flex  duration-500 bg-light bg-slate-700 dark:bg-dark bg-cover`}>
+      <div className={`grid place-items-center h-screen  sm:flex  duration-500 bg-light dark:bg-dark bg-cover`}>
         <div className="w-1/2 sm:w-full sm:h-4/5 md:w-4/5 lg:w-1/2 backdrop-blur-md shadow-xl  ">
           <TabContainer />
         </div>
